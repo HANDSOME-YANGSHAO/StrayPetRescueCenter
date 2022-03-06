4880563484861dfcc7a49a470c8ed5d676620546
cuteSheep <2451228780@qq.com>
2022-03-06 23:18:33 +0800

    fix: 修改登陆按钮的名字

--- src/components/ContainerComps/Header/Drawer/index.vue
+++ src/components/ContainerComps/Header/Drawer/index.vue
@@ -1,12 +1,13 @@
 <template>
   <el-drawer v-model="_drawerVisible" direction="ttb" size="70%" :title="title">
     <template #default>
-      <div>dsadsadsadsadsa</div>
+      <span v-if="_formStatus === 0" class="guide" @click="changeForm">还未注册？点击注册</span>
+      <span v-if="_formStatus === 1" class="guide" @click="changeForm">返回登陆界面</span>
     </template>
     <template #footer>
       <div style="flex: auto">
-        <el-button @click="cancel">cancel</el-button>
-        <el-button type="primary" @click="submit">confirm</el-button>
+        <el-button @click="cancel">取消</el-button>
+        <el-button type="primary" @click="submit">{{ title }}</el-button>
       </div>
     </template>
   </el-drawer>
@@ -27,9 +28,19 @@ const _formStatus = useVModel(props, 'formStatus', emit)
 const title = computed(() => {
   return _formStatus.value === 0 ? '登陆' : _formStatus.value === 1 ? '注册' : '修改个人信息'
 })
+
+const changeForm = () => {
+  if (!_formStatus.value) {
+    _formStatus.value = 1
+  } else {
+    _formStatus.value = 0
+  }
+}
+
 const cancel = () => {
   _drawerVisible.value = false
 }
+
 const refreshHeader = inject('reloadHeader') as any
 const submit = () => {
   // 模拟登陆成功，获得数据
@@ -46,4 +57,10 @@ const submit = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.guide {
+  font-size: 10px;
+  color: #f7811c;
+  cursor: pointer;
+}
+</style>
